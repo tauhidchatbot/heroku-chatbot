d746fb00db910af267edddc4dcbbd0e1e09f86a1
tauhidchatbot <40761572+tauhidchatbot@users.noreply.github.com>
2018-07-06 17:54:03 +0530

    for responce
    done

--- app/controller/botController.js
+++ app/controller/botController.js
@@ -6,6 +6,16 @@ var express = require('express');
 }; */
 
 exports.chatBot = function(req, res) {
-	res.send(req.body);
-	//res.send("Hello Tauhid! Heroku Deploy");
+	/*res.send(req.body);
+	//res.send("Hello Tauhid! Heroku Deploy");*/
+	const data = req.body;
+
+	// Code the task you want to achieve with @data
+	// Read the v2 api documentation of dialogflow : https://dialogflow.com/docs/fulfillment
+	// Using the v2 will become mandatory, Google wrote a guide to migrate from v1 to v2 as v2 is officially released
+
+	const response = {
+		fulfillmentText: "Your webhook works fine !",
+	}
+res.json(response);
 }
